25bf7c80a15dbb0b0ef6ddcbbf09cf33d595f3b6
Jonathon Jongsma <jonathon@quotidian.org>
2023-12-19 09:11:50 -0600

    web: add 'location' combobox to sample info form
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- seedweb/src/html/sample.rs
+++ seedweb/src/html/sample.rs
@@ -5,7 +5,10 @@ use axum::{
     Router,
 };
 use axum_template::RenderHtml;
-use libseed::sample::{self, Sample};
+use libseed::{
+    location::Location,
+    sample::{self, Sample},
+};
 use minijinja::context;
 
 use crate::{error, state::SharedState, CustomKey};
@@ -32,5 +35,14 @@ async fn show_sample(
 ) -> Result<impl IntoResponse, error::Error> {
     let mut builder = sample::build_query(None, Some(id));
     let sample: Sample = builder.build_query_as().fetch_one(&state.dbpool).await?;
-    Ok(RenderHtml(key, state.tmpl, context!(sample => sample)))
+    let locations: Vec<Location> = sqlx::query_as(
+        "SELECT locid, name as locname, description, latitude, longitude FROM seedlocations",
+    )
+    .fetch_all(&state.dbpool)
+    .await?;
+    Ok(RenderHtml(
+        key,
+        state.tmpl,
+        context!(sample => sample, locations => locations),
+    ))
 }
