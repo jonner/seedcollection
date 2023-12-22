454d9dca0ef6074e38a467797e834c97bd6cb8a9
Jonathon Jongsma <jonathon@quotidian.org>
2023-12-21 22:54:56 -0600

    web: implement updating a sample
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/src/html/sample.rs
+++ web/src/html/sample.rs
@@ -1,7 +1,7 @@
 use anyhow::anyhow;
 use axum::{
     extract::{Path, State},
-    response::{IntoResponse, Redirect},
+    response::IntoResponse,
     routing::get,
     Form, Router,
 };
@@ -14,7 +14,7 @@ use libseed::{
 };
 use minijinja::context;
 use serde::{Deserialize, Serialize};
-use sqlx::{sqlite::SqliteQueryResult, QueryBuilder, Sqlite};
+use sqlx::sqlite::SqliteQueryResult;
 
 use crate::{app_url, error, state::SharedState, CustomKey, Message, MessageType};
 
@@ -96,16 +96,21 @@ struct SampleParams {
     notes: Option<String>,
 }
 
-async fn do_insert(
-    params: &SampleParams,
-    state: &SharedState,
-) -> Result<SqliteQueryResult, error::Error> {
+fn validate_sample_params(params: &SampleParams) -> Result<(), anyhow::Error> {
     if params.taxon.is_none() {
         return Err(anyhow!("No taxon specified").into());
     }
     if params.location.is_none() {
         return Err(anyhow!("No location specified").into());
     }
+    Ok(())
+}
+
+async fn do_insert(
+    params: &SampleParams,
+    state: &SharedState,
+) -> Result<SqliteQueryResult, error::Error> {
+    validate_sample_params(params)?;
 
     sqlx::query("INSERT INTO seedsamples (tsn, collectedlocation, month, year, quantity, notes) VALUES (?, ?, ?, ?, ?, ?)")
         .bind(params.taxon)
@@ -163,6 +168,72 @@ async fn insert_sample(
     }
 }
 
-async fn update_sample() -> () {
-    todo!();
+async fn do_update(
+    id: i64,
+    params: &SampleParams,
+    state: &SharedState,
+) -> Result<SqliteQueryResult, error::Error> {
+    validate_sample_params(params)?;
+
+    sqlx::query("Update seedsamples SET tsn=?, collectedlocation=?, month=?, year=?, quantity=?, notes=? WHERE id=?")
+        .bind(params.taxon)
+        .bind(params.location)
+        .bind(params.month)
+        .bind(params.year)
+        .bind(params.quantity)
+        .bind(&params.notes)
+        .bind(id)
+        .execute(&state.dbpool)
+        .await.map_err(|e| e.into())
+}
+
+async fn update_sample(
+    Path(id): Path<i64>,
+    CustomKey(key): CustomKey,
+    State(state): State<SharedState>,
+    Form(params): Form<SampleParams>,
+) -> Result<impl IntoResponse, error::Error> {
+    let locations: Vec<Location> = sqlx::query_as(
+        "SELECT locid, name as locname, description, latitude, longitude FROM seedlocations ORDER BY name ASC",
+    )
+    .fetch_all(&state.dbpool)
+    .await?;
+
+    match do_update(id, &params, &state).await {
+        Err(e) => {
+            let sample: Sample = sample::build_query(Some(Filter::Sample(id)))
+                .build_query_as()
+                .fetch_one(&state.dbpool)
+                .await?;
+
+            Ok(RenderHtml(
+                key + ".partial",
+                state.tmpl,
+                context!(locations => locations,
+                     sample => sample,
+                     message => Message {
+                         r#type: MessageType::Error,
+                         msg: format!("Failed to save sample: {}", e.0.to_string())
+                     },
+                     request => params),
+            ))
+        }
+        Ok(_) => {
+            let sample: Sample = sample::build_query(Some(Filter::Sample(id)))
+                .build_query_as()
+                .fetch_one(&state.dbpool)
+                .await?;
+
+            Ok(RenderHtml(
+                key + ".partial",
+                state.tmpl,
+                context!(locations => locations,
+                         sample => sample,
+                         message => Message {
+                    r#type: MessageType::Success,
+                    msg: format!("Updated sample {}", sample.id)
+                }),
+            ))
+        }
+    }
 }
