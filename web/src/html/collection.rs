73ac7b3df256656eb481b8b508261db24496934f
Jonathon Jongsma <jonathon@quotidian.org>
2023-12-24 22:56:31 -0600

    web: add ability to create a new collection
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/src/html/collection.rs
+++ web/src/html/collection.rs
@@ -1,22 +1,27 @@
+use anyhow::anyhow;
 use axum::{
     extract::{Path, State},
     response::IntoResponse,
-    routing::{get, post},
+    routing::get,
+    Form, Router,
 };
-use axum::{Form, Router};
 use axum_template::RenderHtml;
-use libseed::collection::Collection;
-use libseed::sample::{self, Filter, Sample};
+use libseed::{
+    collection::Collection,
+    empty_string_as_none,
+    sample::{self, Filter, Sample},
+};
 use minijinja::context;
+use serde::{Deserialize, Serialize};
+use sqlx::sqlite::SqliteQueryResult;
 
-use crate::CustomKey;
-use crate::{error, state::SharedState};
+use crate::{app_url, error, state::SharedState, CustomKey, Message, MessageType};
 
 pub fn router() -> Router<SharedState> {
     Router::new()
         .route("/", get(root))
         .route("/list", get(list_collections))
-        .route("/new", post(add_collection))
+        .route("/new", get(new_collection).post(insert_collection))
         .route("/:id/add", get(show_add_sample).post(add_sample))
         .route(
             "/:id",
@@ -38,7 +43,7 @@ async fn list_collections(
     State(state): State<SharedState>,
 ) -> Result<impl IntoResponse, error::Error> {
     let collections: Vec<Collection> =
-        sqlx::query_as("SELECT L.id, L.name, L.description FROM seedcollections L")
+        sqlx::query_as("SELECT id, name, description FROM seedcollections")
             .fetch_all(&state.dbpool)
             .await?;
     Ok(RenderHtml(
@@ -48,8 +53,71 @@ async fn list_collections(
     ))
 }
 
-async fn add_collection() -> impl IntoResponse {
-    "Add collection"
+async fn new_collection(
+    CustomKey(key): CustomKey,
+    State(state): State<SharedState>,
+) -> Result<impl IntoResponse, error::Error> {
+    Ok(RenderHtml(key, state.tmpl, ()))
+}
+
+#[derive(Deserialize, Serialize)]
+struct CollectionParams {
+    name: String,
+    #[serde(deserialize_with = "empty_string_as_none")]
+    description: Option<String>,
+}
+
+async fn do_insert(
+    params: &CollectionParams,
+    state: &SharedState,
+) -> Result<SqliteQueryResult, error::Error> {
+    if params.name.is_empty() {
+        return Err(anyhow!("No name specified").into());
+    }
+    sqlx::query("INSERT INTO seedcollections (name, description) VALUES (?, ?)")
+        .bind(params.name.clone())
+        .bind(params.description.clone())
+        .execute(&state.dbpool)
+        .await
+        .map_err(|e| e.into())
+}
+
+async fn insert_collection(
+    CustomKey(key): CustomKey,
+    State(state): State<SharedState>,
+    Form(params): Form<CollectionParams>,
+) -> Result<impl IntoResponse, error::Error> {
+    match do_insert(&params, &state).await {
+        Err(e) => Ok(RenderHtml(
+            key + ".partial",
+            state.tmpl,
+            context!( message => Message {
+                r#type: MessageType::Error,
+                msg: format!("Failed to save collection: {}", e.0.to_string())
+            },
+            request => params),
+        )),
+        Ok(result) => {
+            let id = result.last_insert_rowid();
+            let collection: Collection =
+                sqlx::query_as("SELECT id, name, description FROM seedcollections WHERE id=?")
+                    .bind(id)
+                    .fetch_one(&state.dbpool)
+                    .await?;
+
+            let collectionurl = app_url(format!("/collection/{}", collection.id));
+            Ok(RenderHtml(
+                key + ".partial",
+                state.tmpl,
+                context!(message => Message {
+                    r#type: MessageType::Success,
+                    msg: format!(r#"Added new collection <a href="{}">{}: {}</a> to the database"#,
+                                 collectionurl,
+                                 collection.id, collection.name)
+                }),
+            ))
+        }
+    }
 }
 
 async fn show_collection(
@@ -58,7 +126,7 @@ async fn show_collection(
     State(state): State<SharedState>,
 ) -> Result<impl IntoResponse, error::Error> {
     let mut c: Collection =
-        sqlx::query_as("SELECT L.id, L.name, L.description FROM seedcollections L WHERE id=?")
+        sqlx::query_as("SELECT id, name, description FROM seedcollections WHERE id=?")
             .bind(id)
             .fetch_one(&state.dbpool)
             .await?;
