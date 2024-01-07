6749a458fdcf8cfd4361cfddbd2bab06f559576e
Jonathon Jongsma <jonathon@quotidian.org>
2024-01-06 20:49:26 -0600

    template: Add sample-related macros to separate file
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/src/html/sample.rs
+++ web/src/html/sample.rs
@@ -83,7 +83,7 @@ async fn show_sample(
 ) -> Result<impl IntoResponse, error::Error> {
     let mut sample = Sample::fetch(id, &state.dbpool).await?;
     let collections: Vec<Collection> = sqlx::query_as(
-        r#"SELECT C.id, C.name, C.description FROM sc_collections C INNER JOIN sc_collection_samples CS
+        r#"SELECT C.id, C.name, C.description, C.userid FROM sc_collections C INNER JOIN sc_collection_samples CS
         ON C.id == CS.collectionid WHERE CS.sampleid = ?"#)
         .bind(id)
         .fetch_all(&state.dbpool)
