602557157fa676e4d3fe2d8ced15b444368baf5c
Jonathon Jongsma <jonathon@quotidian.org>
2024-07-30 22:51:40 -0500

    seedctl: move ProjectCommands to a separate file
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- seedctl/src/commands/mod.rs
+++ seedctl/src/commands/mod.rs
@@ -1,2 +1,3 @@
+pub mod projects;
 pub mod samples;
 pub mod sources;
--- seedctl/src/main.rs
+++ seedctl/src/main.rs
@@ -1,8 +1,7 @@
 use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use libseed::{
-    loadable::{ExternalRef, Loadable},
-    project::Project,
+    loadable::Loadable,
     taxonomy::{filter_by, Germination, Taxon},
     user::{User, UserStatus},
     Error::DatabaseRowNotFound,
@@ -79,94 +78,9 @@ async fn main() -> Result<()> {
             println!("Logged in as user '{}'", cfg.username);
             Ok(())
         }
-        Commands::Projects { command } => match command {
-            ProjectCommands::List {} => {
-                let projects = Project::load_all(None, &dbpool).await?;
-                let mut table = Table::new(projects.iter().map(|val| ProjectRow::new(&val)));
-                println!("{}\n", table.styled());
-                println!("{} records found", table.count_rows());
-                Ok(())
-            }
-            ProjectCommands::Add {
-                name,
-                description,
-                userid,
-            } => {
-                let mut project = Project::new(name, description, userid.unwrap_or(user.id));
-                let id = project.insert(&dbpool).await?.last_insert_rowid();
-                let project = Project::load(id, &dbpool).await?;
-                println!("Added project to database:");
-                println!("{}: {}", project.id, project.name);
-                Ok(())
-            }
-            ProjectCommands::Modify {
-                id,
-                name,
-                description,
-            } => {
-                let mut project = Project::load(id, &dbpool).await?;
-                if let Some(name) = name {
-                    project.name = name
-                }
-                if let Some(description) = description {
-                    project.description = Some(description);
-                }
-                project.update(&dbpool).await?;
-                println!("Modified project...");
-                Ok(())
-            }
-            ProjectCommands::Remove { id } => {
-                Project::delete_id(&id, &dbpool).await?;
-                println!("Removed project {id}");
-                Ok(())
-            }
-            ProjectCommands::AddSample { project, sample } => {
-                let mut project = Project::load(project, &dbpool).await?;
-                project
-                    .allocate_sample(ExternalRef::Stub(sample), &dbpool)
-                    .await?;
-                println!("Added sample to project");
-                Ok(())
-            }
-            ProjectCommands::RemoveSample { project, sample } => {
-                sqlx::query!(
-                    r#"DELETE FROM sc_project_samples WHERE projectid=? AND sampleid=?"#,
-                    project,
-                    sample,
-                )
-                .execute(&dbpool)
-                .await?;
-                println!("Removed sample from project");
-                Ok(())
-            }
-            ProjectCommands::Show { id, full } => match Project::load(id, &dbpool).await {
-                Ok(mut projectinfo) => {
-                    projectinfo.load_samples(None, None, &dbpool).await?;
-                    let mut table = match full {
-                        true => Table::new(
-                            projectinfo
-                                .allocations
-                                .iter()
-                                .map(|alloc| AllocationRowFull::new(alloc).unwrap()),
-                        ),
-                        false => Table::new(
-                            projectinfo
-                                .allocations
-                                .iter()
-                                .map(|alloc| AllocationRow::new(alloc).unwrap()),
-                        ),
-                    };
-                    println!("{}\n", table.styled());
-                    println!("{} records found", table.count_rows());
-                    Ok(())
-                }
-                Err(DatabaseRowNotFound(_)) => {
-                    println!("Project {id} not found");
-                    Ok(())
-                }
-                Err(e) => Err(e.into()),
-            },
-        },
+        Commands::Projects { command } => {
+            commands::projects::handle_command(command, user, &dbpool).await
+        }
         Commands::Sources { command } => {
             commands::sources::handle_command(command, user, &dbpool).await
         }
