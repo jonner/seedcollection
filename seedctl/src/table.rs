523cd433b16040f3805e18944346fcac978e80df
Jonathon Jongsma <jonathon@quotidian.org>
2024-08-12 11:41:18 -0500

    seedctl: update tabled to 0.16.0
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- seedctl/src/table.rs
+++ seedctl/src/table.rs
@@ -7,7 +7,7 @@ pub trait SeedctlTable {
 impl SeedctlTable for Table {
     fn styled(&mut self) -> &mut Self {
         use tabled::settings::{object::Segment, width::Width, Modify, Style};
-        let m = Modify::new(Segment::all()).with(Width::wrap(60).keep_words());
+        let m = Modify::new(Segment::all()).with(Width::wrap(60).keep_words(true));
         self.with(m).with(Style::psql())
     }
 }
