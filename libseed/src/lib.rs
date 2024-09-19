786470a2c9be9544463f755bcca9a58f1facbe90
Jonathon Jongsma <jonathon@quotidian.org>
2024-09-19 16:45:28 -0500

    lib: make empty_string_as_none() more useful
    Rather than requiring that the deserialize type implement FromStr, just
    require that it is deserializable.
    
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- libseed/src/lib.rs
+++ libseed/src/lib.rs
@@ -1,8 +1,8 @@
 //! This is a library that provides objects and functionality to help you manage a native seed
 //! collection and keep track of everything inside of a database.
 
+use serde::de::IntoDeserializer;
 use serde::{Deserialize, Deserializer};
-use std::str::FromStr;
 
 pub mod error;
 pub mod filter;
@@ -19,14 +19,11 @@ pub use error::Result;
 pub fn empty_string_as_none<'de, D, T>(de: D) -> Result<Option<T>, D::Error>
 where
     D: Deserializer<'de>,
-    T: FromStr,
-    T::Err: std::fmt::Display,
+    T: Deserialize<'de>,
 {
     let opt = Option::<String>::deserialize(de)?;
     match opt.as_deref() {
         None | Some("") => Ok(None),
-        Some(s) => FromStr::from_str(s)
-            .map_err(serde::de::Error::custom)
-            .map(Some),
+        Some(s) => T::deserialize(s.into_deserializer()).map(Some),
     }
 }
