9e3f0d9128857c1eeb23cb7cbb6dd612e536c59e
Jonathon Jongsma <jonathon@quotidian.org>
2025-03-17 16:51:10 -0500

    seedctl: split user admin commands into separate functions
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- seedctl/src/commands/admin/user.rs
+++ seedctl/src/commands/admin/user.rs
@@ -13,8 +13,98 @@ use tokio::fs;
 
 use std::path::PathBuf;
 
-/// Get a password from the user, either from the file at the provided path, or
-/// by reading input from stdin.
+pub(crate) async fn handle_command(dbpath: Option<PathBuf>, command: UserCommands) -> Result<()> {
+    let db = Database::open(dbpath.ok_or_else(|| anyhow!("No database specified"))?).await?;
+    match command {
+        UserCommands::List { output } => list_users(&db, output).await,
+        UserCommands::Add {
+            username,
+            email,
+            passwordfile,
+        } => add_user(&db, username, email, passwordfile).await,
+        UserCommands::Remove { id } => remove_user(&db, id).await,
+        UserCommands::Modify {
+            id,
+            username,
+            change_password,
+            passwordfile,
+        } => modify_user(&db, id, username, change_password, passwordfile).await,
+    }
+}
+
+async fn modify_user(
+    db: &Database,
+    id: i64,
+    username: Option<String>,
+    change_password: bool,
+    passwordfile: Option<PathBuf>,
+) -> Result<()> {
+    let mut user = User::load(id, db).await?;
+    if let Some(username) = username {
+        user.username = username;
+    }
+    if change_password {
+        let password = get_password(passwordfile).await?;
+        user.change_password(&password)?;
+    }
+    user.update(db)
+        .await
+        .map(|_| ())
+        .with_context(|| "Failed to modify user")
+}
+
+async fn remove_user(db: &Database, id: i64) -> Result<()> {
+    match inquire::Confirm::new("Really remove user?")
+        .with_default(false)
+        .prompt()?
+    {
+        true => User::delete_id(&id, db)
+            .await
+            .map(|_| ())
+            .with_context(|| "failed to remove user"),
+        false => Ok(()),
+    }
+}
+
+async fn add_user(
+    db: &Database,
+    username: Option<String>,
+    email: Option<String>,
+    passwordfile: Option<PathBuf>,
+) -> Result<()> {
+    let username = username
+        .or_else(|| inquire::Text::new("Username:").prompt().ok())
+        .ok_or_else(|| anyhow!("No username specified"))?;
+    let email = email
+        .or_else(|| inquire::Text::new("Email Address:").prompt().ok())
+        .ok_or_else(|| anyhow!("No email address specified"))?;
+    let password = get_password(passwordfile).await?;
+    // hash the password
+    let pwhash = User::hash_password(&password)?;
+    let mut user = User::new(
+        username.clone(),
+        email.clone(),
+        pwhash,
+        UserStatus::Unverified,
+        None,
+        None,
+        None,
+    );
+    let id = user.insert(db).await?.last_insert_rowid();
+    println!("Added user to database:");
+    println!("{}: {}", id, username);
+    Ok(())
+}
+
+async fn list_users(db: &Database, output: crate::OutputOptions) -> Result<()> {
+    let users = User::load_all(db).await?;
+    let str = output::format_seq(users.iter().map(UserRow::new), output.format)?;
+    println!("{str}");
+    Ok(())
+}
+
+/// Utility function to get a password from the user, either from the file at
+/// the provided path, or by reading input from stdin.
 async fn get_password(path: Option<PathBuf>) -> Result<String> {
     let password = match path {
         None => {
@@ -26,76 +116,4 @@ async fn get_password(path: Option<PathBuf>) -> Result<String> {
         Some(f) => fs::read_to_string(f).await?,
     };
     Ok(password.trim().to_string())
-}
-
-pub(crate) async fn handle_command(dbpath: Option<PathBuf>, command: UserCommands) -> Result<()> {
-    let db = Database::open(dbpath.ok_or_else(|| anyhow!("No database specified"))?).await?;
-    match command {
-        UserCommands::List { output } => {
-            let users = User::load_all(&db).await?;
-            let str = output::format_seq(users.iter().map(UserRow::new), output.format)?;
-            println!("{str}");
-            Ok(())
-        }
-        UserCommands::Add {
-            username,
-            email,
-            passwordfile,
-        } => {
-            let username = username
-                .or_else(|| inquire::Text::new("Username:").prompt().ok())
-                .ok_or_else(|| anyhow!("No username specified"))?;
-            let email = email
-                .or_else(|| inquire::Text::new("Email Address:").prompt().ok())
-                .ok_or_else(|| anyhow!("No email address specified"))?;
-            let password = get_password(passwordfile).await?;
-            // hash the password
-            let pwhash = User::hash_password(&password)?;
-
-            let mut user = User::new(
-                username.clone(),
-                email.clone(),
-                pwhash,
-                UserStatus::Unverified,
-                None,
-                None,
-                None,
-            );
-            let id = user.insert(&db).await?.last_insert_rowid();
-            println!("Added user to database:");
-            println!("{}: {}", id, username);
-            Ok(())
-        }
-        UserCommands::Remove { id } => {
-            match inquire::Confirm::new("Really remove user?")
-                .with_default(false)
-                .prompt()?
-            {
-                true => User::delete_id(&id, &db)
-                    .await
-                    .map(|_| ())
-                    .with_context(|| "failed to remove user"),
-                false => Ok(()),
-            }
-        }
-        UserCommands::Modify {
-            id,
-            username,
-            change_password,
-            passwordfile,
-        } => {
-            let mut user = User::load(id, &db).await?;
-            if let Some(username) = username {
-                user.username = username;
-            }
-            if change_password {
-                let password = get_password(passwordfile).await?;
-                user.change_password(&password)?;
-            }
-            user.update(&db)
-                .await
-                .map(|_| ())
-                .with_context(|| "Failed to modify user")
-        }
-    }
 }
